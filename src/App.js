86c20374909ee618a38be18daa299188ed5f949b
batuhaandogan <tipala_1909@hotmail.com>
2018-09-30 14:56:50 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+constructor(props) {
+super(props)
+this.state = {
+   message: 'Hello Ironhack',
+   newTodo: "",
+   todos: "",
+   
+ }
+}
+
+newTodoChanged = (event) => {
+  // console.log(event.target.value)
+  this.setState({
+    newTodo: event.target.value,
+  })
+}
+
+formSubmitted = (event) => {
+  event.preventDefault();
+  // console.log(this.state.newTodo);
+  this.setState({
+    todos: [...this.state.todos, {
+      title: this.setState.newTodo,
+      done: false,
+    }]
+  })
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App"> 
+      <h1>{this.state.message}</h1>
+
+        <form onSubmit={this.formSubmitted.bind(this)}>
+             <label htmlFor="newTodo">New Todo</label>
+             <input onChange={(event)=> this.newTodoChanged(event)} name="newTodo"  id="newTodo"/>
+             <button type="submit">Add todo</button>
+        </form>
+
       </div>
     );
   }
